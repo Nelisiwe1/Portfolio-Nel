a10fb094cf7edd11dcff51d280e7e5a78d27960f
Nelisiwe1 <nelisiwengqeme3@gmail.com>
2024-02-12 00:19:30 +0200

    navbar

--- projectName/src/Navbar.jsx
+++ projectName/src/Navbar.jsx
@@ -3,8 +3,8 @@ import { NavLink } from "react-router-dom";
 const Navbar = () => {
   return (
     <header className='header'>
-      <NavLink to='/'>
-        {/* <img src={logo} alt='logo' className='w-18 h-18 object-contain' /> */}
+      <NavLink to='/home' className={({ isActive }) => isActive ? "text-blue-600" : "text-black" }>
+          Home
       </NavLink>
       <nav className='flex text-lg gap-7 font-medium'>
         <NavLink to='/about' className={({ isActive }) => isActive ? "text-blue-600" : "text-black" }>
